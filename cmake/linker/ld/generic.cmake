7f61391885eb33261fa5c735e3877ee1f34c847c
miracle <378479645@qq.com>
2021-08-12 18:26:09 +0800

    add kconfig.include

---
+++ cmake/linker/ld/generic.cmake
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+
+find_program(CMAKE_LINKER ld.bfd ${find_program_gcc_args})
---
+++ cmake/linker/lld/generic.cmake
@@ -0,0 +1,3 @@
+# SPDX-License-Identifier: Apache-2.0
+
+find_program(CMAKE_LINKER ld.lld ${find_program_gcc_args})

57d99799ab7f2d519fc38c481336f469943000ca
miracle <378479645@qq.com>
2021-08-24 10:28:57 +0800

    fix version not compile_h

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -1,17 +1,13 @@
 
 #include <generated/asm-offsets.h>
 
-#include <muslc/muslc.h>
 
-#include <misc/misc.h>
 
-extern void test1(int d);
 
 int bbb;
 void start_kernel(void)
 {
-	test1(2);
-	muslc();
+
 
 	bbb = CONFIG_NR_CPUS;
 }

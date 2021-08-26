02ef029c7abcb367e4e08ac4bb6fd442404f1166
miracle <378479645@qq.com>
2021-08-27 00:01:27 +0800

    ss

--- kernel/init/main.c
+++ kernel/init/main.c
@@ -1,9 +1,18 @@
+/*
+ *  linux/init/main.c
+ *
+ *  Copyright (C) 1991, 1992  Linus Torvalds
+ *
+ *  GK 2/5/95  -  Changed to support mounting root fs via NFS
+ *  Added initrd & change_root: Werner Almesberger & Hans Lermen, Feb '96
+ *  Moan early if gcc is old, avoiding bogus kernels - Paul Gortmaker, May '96
+ *  Simplified starting of init:  Michael A. Griffith <grif@acm.org>
+ */
+#include <sel4m/init.h>
+#include <sel4m/compiler.h>
+#include <sel4m/linkage.h>
 
-#include <generated/asm-offsets.h>
-
-
-void start_kernel(void)
+asmlinkage __visible void __init start_kernel(void)
 {
 
 }
-

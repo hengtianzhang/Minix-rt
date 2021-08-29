02d37f108b093b02a5068c98dfcc987a32736c3f
miracle <378479645@qq.com>
2021-08-29 14:15:22 +0800

    s

---
+++ kernel/arch/arm64/include/asm/string.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2013 ARM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_STRING_H_
+#define __ASM_STRING_H_
+
+#include <sel4m/types.h>
+
+#define __HAVE_ARCH_STRLEN
+extern size_t strlen(const char *);
+
+#endif /* !__ASM_STRING_H_ */

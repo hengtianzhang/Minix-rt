5de56499f13cb795c07133c8014f48cc885dbce0
miracle <378479645@qq.com>
2021-08-31 10:15:03 +0800

    sdad

--- kernel/arch/arm64/include/asm/barrier.h
+++ kernel/arch/arm64/include/asm/barrier.h
@@ -18,44 +18,6 @@
 #ifndef __ASM_BARRIER_H_
 #define __ASM_BARRIER_H_
 
-#ifndef __ASSEMBLY__
+#include <asm/base/barrier.h>
 
-#define __nops(n)	".rept	" #n "\nnop\n.endr\n"
-#define nops(n)		asm volatile(__nops(n))
-
-#define sev()		asm volatile("sev" : : : "memory")
-#define wfe()		asm volatile("wfe" : : : "memory")
-#define wfi()		asm volatile("wfi" : : : "memory")
-
-#define isb()		asm volatile("isb" : : : "memory")
-#define dmb(opt)	asm volatile("dmb " #opt : : : "memory")
-#define dsb(opt)	asm volatile("dsb " #opt : : : "memory")
-
-#define psb_csync()	asm volatile("hint #17" : : : "memory")
-#define csdb()		asm volatile("hint #20" : : : "memory")
-
-#define spec_bar()	asm volatile("dsb nsh\nisb\n")
-
-#define mb()		dsb(sy)
-#define rmb()		dsb(ld)
-#define wmb()		dsb(st)
-
-#define dma_rmb()	dmb(oshld)
-#define dma_wmb()	dmb(oshst)
-
-#define __smp_mb()	dmb(ish)
-#define __smp_rmb()	dmb(ishld)
-#define __smp_wmb()	dmb(ishst)
-
-#define read_barrier_depends()		do { } while(0)
-#define smp_read_barrier_depends()	do { } while(0)
-
-#define set_mb(var, value)	do { var = value; smp_mb(); } while (0)
-#define nop()		asm volatile("nop");
-
-#include <asm/processor.h>
-
-#include <asm-generic/barrier.h>
-
-#endif	/* !__ASSEMBLY__ */
 #endif /* !__ASM_BARRIER_H_ */

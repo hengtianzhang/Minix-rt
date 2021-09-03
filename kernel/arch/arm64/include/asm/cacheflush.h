cc3d33c60b36e15afdcfe938df298c386d2e1727
miracle <378479645@qq.com>
2021-09-03 17:40:07 +0800

    s

---
+++ kernel/arch/arm64/include/asm/cacheflush.h
@@ -0,0 +1,24 @@
+/*
+ * Based on arch/arm/include/asm/cacheflush.h
+ *
+ * Copyright (C) 1999-2002 Russell King.
+ * Copyright (C) 2012 ARM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __ASM_CACHEFLUSH_H_
+#define __ASM_CACHEFLUSH_H_
+
+extern void __inval_dcache_area(void *addr, size_t len);
+
+#endif /* !__ASM_CACHEFLUSH_H_ */

38e0daf47097d0850c08eaa3b296edb069611dae
miracle <378479645@qq.com>
2021-09-11 22:28:23 +0800

    dasdas

--- kernel/core/object/tcb/pid.c
+++ kernel/core/object/tcb/pid.c
@@ -1,4 +1,5 @@
 #include <base/common.h>
+#include <base/init.h>
 
 #include <sel4m/sched.h>
 #include <sel4m/sched/idle.h>
@@ -10,18 +11,18 @@ static struct pid_struct *pid_find(pid_t pid)
 {
 	struct rb_node *node = pid_root.rb_node;
 
-    while (node) {
-        struct pid_struct *data = container_of(node, struct pid_struct, node);
-    
-    	if (pid < data->pid)
+	while (node) {
+		struct pid_struct *data = container_of(node, struct pid_struct, node);
+	
+		if (pid < data->pid)
 			node = node->rb_left;
 		else if (pid > data->pid)
 			node = node->rb_right;
 		else
 			return data;
-    }
+	}
 
-    return NULL;
+	return NULL;
 }
 
 /**
@@ -30,13 +31,13 @@ static struct pid_struct *pid_find(pid_t pid)
  */
 struct task_struct *find_process_by_pid(pid_t pid)
 {
-    struct pid_struct *pids;
+	struct pid_struct *pids;
 
-    pids = pid_find(pid);
-    if (!pids)
-        return NULL;
+	pids = pid_find(pid);
+	if (!pids)
+		return NULL;
 
-    return container_of(pids, struct task_struct, pid);
+	return container_of(pids, struct task_struct, pid);
 }
 
 static bool pid_insert(struct pid_struct *data)
@@ -65,19 +66,67 @@ static bool pid_insert(struct pid_struct *data)
 
 bool insert_process_by_pid(struct task_struct *tsk)
 {
-    if (!tsk)
-        return false;
+	if (!tsk)
+		return false;
 
-    return pid_insert(&tsk->pid);
+	return pid_insert(&tsk->pid);
 }
 
-void process_pid_init(void)
+bool remove_pid_by_process(struct task_struct *tsk)
 {
-    int cpu;
+	struct pid_struct *pids;
 
-    for_each_possible_cpu(cpu) {
-        idle_threads[cpu].pid.pid = -cpu;
-        if (!insert_process_by_pid(&idle_threads[cpu]))
-            BUG();
-    }
+	if (!tsk)
+		return false;
+
+	pids = pid_find(tsk->pid.pid);
+	if (!pids)
+		return false;
+
+	rb_erase(&pids->node, &pid_root);
+
+	return true;
+}
+
+pid_t pid_first(void)
+{
+	struct rb_node *node;
+	struct pid_struct *pids;
+
+	node = rb_first(&pid_root);
+	if (!node)
+		return INT_MAX;
+
+	pids = rb_entry(node, struct pid_struct, node);
+
+	return pids->pid;
+}
+
+pid_t pid_next(pid_t pid)
+{
+	struct rb_node *node;
+	struct pid_struct *pids;
+
+	pids = pid_find(pid);
+	if (!pids)
+		return INT_MAX;
+
+	node = rb_next(&pids->node);
+	if (!node)
+		return INT_MAX;
+
+	pids = rb_entry(node, struct pid_struct, node);
+
+	return pids->pid;
+}
+
+void __init process_pid_init(void)
+{
+	int cpu;
+
+	for_each_possible_cpu(cpu) {
+		idle_threads[cpu].pid.pid = -cpu;
+		if (!insert_process_by_pid(&idle_threads[cpu]))
+			BUG();
+	}
 }
--- kernel/include/sel4m/object/pid.h
+++ kernel/include/sel4m/object/pid.h
@@ -5,13 +5,19 @@
 #include <base/rbtree.h>
 
 struct pid_struct {
-    pid_t   pid;
-    struct rb_node  node;
+	pid_t   pid;
+	struct rb_node  node;
 };
 
 struct task_struct *find_process_by_pid(pid_t pid);
 bool insert_process_by_pid(struct task_struct *tsk);
+bool remove_pid_by_process(struct task_struct *tsk);
 
 void process_pid_init(void);
+pid_t pid_first(void);
+pid_t pid_next(pid_t pid);
+
+#define for_each_pid(pid)   \
+	for (pid = pid_first(); pid != INT_MAX; pid = pid_next(pid))
 
 #endif /* !__SEL4M_OBJECT_PID_H_ */

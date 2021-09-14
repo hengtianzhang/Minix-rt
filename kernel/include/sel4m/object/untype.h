451739905ce8633a75019c59e626d72fdbc3d4f5
miracle <378479645@qq.com>
2021-09-14 17:49:05 +0800

    add protection map

--- kernel/include/sel4m/object/untype.h
+++ kernel/include/sel4m/object/untype.h
@@ -6,6 +6,17 @@
 #include <asm/pgtable-types.h>
 #include <asm/mmu.h>
 
+/*
+ * vm_flags in vm_area_struct, see mm_types.h.
+ * When changing, update also include/trace/events/mmflags.h
+ */
+#define VM_NONE		0x00000000
+
+#define VM_READ		0x00000001	/* currently active flags */
+#define VM_WRITE	0x00000002
+#define VM_EXEC		0x00000004
+#define VM_SHARED	0x00000008
+
 struct untype_struct {
 	unsigned long nr_pages;
 	unsigned long nr_used_pages;

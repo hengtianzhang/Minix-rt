1886059f46b6b9e6f6cfc744d45a77d3f4b78e37
miracle <378479645@qq.com>
2021-09-15 21:41:31 +0800

    ad

--- kernel/core/sched/fair.c
+++ kernel/core/sched/fair.c
@@ -483,11 +483,10 @@ static void put_prev_entity(struct cfs_rq *cfs_rq, struct sched_entity *prev)
 	 * If still on the runqueue then deactivate_task()
 	 * was not called and update_curr() has to be done:
 	 */
-	if (prev->on_rq)
+	if (prev->on_rq) {
 		update_curr(cfs_rq);
-
-	if (prev->on_rq)
 		__enqueue_entity(cfs_rq, prev);
+	}
 
 	cfs_rq->curr = NULL;
 }

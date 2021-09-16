954b54aa2715d46aa7a9934a924318a265e03beb
miracle <378479645@qq.com>
2021-09-16 11:25:05 +0800

    sss

--- kernel/core/sched/core.c
+++ kernel/core/sched/core.c
@@ -2560,13 +2560,11 @@ need_resched:
  */
 asmlinkage void __sched preempt_schedule(void)
 {
-	struct thread_info *ti = current_thread_info();
-
 	/*
 	 * If there is a non-zero preempt_count or interrupts are disabled,
 	 * we do not want to preempt the current task. Just return..
 	 */
-	if (likely(ti->preempt_count || irqs_disabled()))
+	if (likely(!preemptible()))
 		return;
 
 	do {

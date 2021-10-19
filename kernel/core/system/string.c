7f0bc35f1cee34ddca2488f9f31a141bc3ace835
miracle <378479645@qq.com>
2021-10-19 21:55:14 +0800

    ad

--- kernel/core/system/string.c
+++ kernel/core/system/string.c
@@ -2,17 +2,71 @@
 #include <minix_rt/sched.h>
 #include <minix_rt/page.h>
 #include <minix_rt/mmap.h>
+#include <minix_rt/slab.h>
 
 static int message_memcpy(const void *dst, const void *src,
 						int size, pid_t dst_pid, pid_t src_pid)
 {
-	return -3;
+	int ret = 0;
+	struct task_struct *dst_tsk, *src_tsk;
+	void *tmp_buf;
+	struct vm_area_struct *vma;
+
+	dst_tsk = pid_find_process_by_pid(dst_pid);
+	if (!dst_tsk) {
+		ret = -ESRCH;
+		goto out;
+	}
+
+	src_tsk = pid_find_process_by_pid(src_pid);
+	if (!src_tsk) {
+		ret = -ESRCH;
+		goto out;
+	}
+
+	/*
+	 * The dest task may be in the Running state
+	 */
+	if (!src_tsk->state) {
+		ret = -EBUSY;
+		goto out;
+	}
+
+	if (!size) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	tmp_buf = kmalloc(size, GFP_KERNEL | GFP_ZERO);
+	if (!tmp_buf) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	vma = mmap_find_vma_area((unsigned long)src, src_tsk->mm);
+	if (!vma) {
+		ret = -EPERM;
+		goto free_out;
+	}
+
+free_out:
+	kfree(tmp_buf);
+out:
+	return ret;
 }
 
 void system_string(endpoint_t ep, message_t *m)
 {
 	int ret;
 	int direct = m->m_sys_string.direct;
+	struct task_struct *tsk;
+
+	tsk = pid_find_process_by_pid(m->m_source);
+	BUG_ON(!tsk);
+	if (unlikely(!(tsk->flags & PF_SYSTEMSERVICE))) {
+		m->m_sys_string.retval = -EACCES;
+		return ;
+	}
 
 	switch (direct) {
 		case DIRECT_CPY_FROM:
